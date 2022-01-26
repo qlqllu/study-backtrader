c4fcd2712ec9481eb9ca72840266cb4671f45e96
Junshan Liu <junshan_liu@esri.com>
2022-01-26 19:37:21 +0800

    st

--- stats/stat_day.py
+++ stats/stat_day.py
@@ -7,14 +7,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# class TestStrategy(bt.Strategy):
-#   def stop(self):
-#     diff = np.array(self.data.close) - np.array(self.data.open)
-#     self.pUp = np.extract(diff > 0, diff)
-#     self.pDown = np.extract(diff < 0, diff)
-#     print(f'Up: {np.quantile(self.pUp, [0.2, 0.4, 0.6, 0.8])}')
-#     print(f'Down: {np.quantile(self.pDown, [0.2, 0.4, 0.6, 0.8])}')
-
 if __name__ == '__main__':
 
   result = dict(id=[], low=[], middle=[], high=[])
@@ -22,11 +14,16 @@ if __name__ == '__main__':
   files = os.listdir('./stock_data')
   for file in files:
     stockId = file.split('.')[1]
-    if stockId.startswith('3') or stockId.startswith('8'):
+    if stockId.startswith('3') or stockId.startswith('4') or stockId.startswith('8'):
       continue
 
     print('Load data ' + stockId)
+
     data = pd.read_csv('./stock_data/' + file, parse_dates=[1])
+
+    if (datetime.datetime.now() - data.dt[0]).days < 365 * 2:
+      continue
+
     open = np.array(data.Open)
     close = np.array(data.Close)
     diff = close - open
@@ -41,9 +38,9 @@ if __name__ == '__main__':
 
   resultData = pd.DataFrame(result)
   resultData.to_csv('./up_stat.csv')
-  sns.lineplot(data=resultData, x='id', y='low')
-  sns.lineplot(data=resultData, x='id', y='middle')
-  sns.lineplot(data=resultData, x='id', y='high')
+  sns.relplot(data=resultData, x='id', y='low')
+  sns.relplot(data=resultData, x='id', y='middle')
+  sns.relplot(data=resultData, x='id', y='high')
   plt.show()
       # sns.lineplot(data=data, x='dt', y='Close')
       # plt.show()
