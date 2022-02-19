55b0f78c0d6d8a0959c5f15687f24db24ef91f18
Junshan Liu <junshan_liu@esri.com>
2022-02-19 08:03:18 +0800

    update path

--- run_s.py
+++ run_s.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
   strats = cerebro.addstrategy(MAStrategy)
 
   data = bt.feeds.GenericCSVData(
-      dataname='E:\\github\\C3-Data-Science\\backtest\\datas\\stock\\zh_a\\000001.csv',
+      dataname='./stock_data/0.000001.csv',
       datetime=1,
       open=2,
       close=3,
