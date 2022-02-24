b9eab3d5b60de5a2147fb78138a416ef1603867e
Junshan Liu <junshan_liu@esri.com>
2022-02-24 23:45:32 +0800

    low price

--- stats/stat_low_price.py
+++ stats/stat_low_price.py
@@ -9,25 +9,37 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-  result = dict(id=[])
+  result = dict(id=[], name=[])
+  data_folder = 'F:\\DS\\C3-Data-Science\\backtest\\datas\\stock\\'
 
-  files = os.listdir('./stock_data')
+  stock_list = pd.read_csv(data_folder + 'zh_a_stock_list.csv', index_col=2, dtype={'代码': str})
+
+  files = os.listdir(f'{data_folder}zh_a\\')
+  i = 0
   for file in files:
-    stockId = file.split('.')[1]
-    if stockId.startswith('3') or stockId.startswith('4') or stockId.startswith('8'):
+    stock_id = file.split('.')[0]
+    if not stock_id in stock_list.index.values:
       continue
+    stock_name = stock_list.loc[stock_id, '名称']
+    # if stockId.startswith('3') or stockId.startswith('4') or stockId.startswith('8'):
+    #   continue
 
-    print('Load data ' + stockId)
+    i += 1
+    print(f'Load data {i}: {stock_id}, {stock_name}')
 
-    data = pd.read_csv('./stock_data/' + file, parse_dates=[1])
+    data = pd.read_csv(f'{data_folder}zh_a\\{file}', parse_dates=[1])
 
     date_3y_before = datetime.datetime.now() - datetime.timedelta(365*3)
-    data = data[data['dt'] > date_3y_before]
+    data = data[data['datetime'] > date_3y_before]
 
-    close = np.array(data.Close)
+    if len(data) == 0:
+      continue
+
+    close = np.array(data.close)
     low_price = np.max(close) * 0.2
     if low_price >= close[-1]:
-      result['id'].append(stockId)
+      result['id'].append(stock_id)
+      result['name'].append(stock_name)
 
   resultData = pd.DataFrame(result)
   resultData.to_csv('./low_price.csv')
